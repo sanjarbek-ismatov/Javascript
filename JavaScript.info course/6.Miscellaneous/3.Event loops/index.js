55f49239fd0de36950e59125cb69089194413451
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-07-09 09:20:09 +0500

    learn: Event loops

--- JavaScript.info course/6.Miscellaneous/3.Event loops/index.js
+++ JavaScript.info course/6.Miscellaneous/3.Event loops/index.js
@@ -1,21 +1,53 @@
 const callBtn = document.getElementById("call");
+// let i = 0;
+//
+// function count(startTime) {
+//   do {
+//     i++;
+//   } while (i % 1e6 != 0);
+//   if (i == 1e9)
+//     alert(
+//       "Hey bro, I've just finished it within " + (Date.now() - startTime) + "ms"
+//     );
+//   else setTimeout(() => count(startTime));
+// }
 
-function count() {
-  const startTime = Date.now();
-  let i = 0;
+// function count(startTime) {
+//   do {
+//     i++;
+//   } while (i <= 1e9);
+//   alert(
+//     "Hey bro, I've just finished it within " + (Date.now() - startTime) + "ms"
+//   );
+// }
+//
+// callBtn.onclick = function () {
+//   i = 0;
+//   const startTime = Date.now();
+//   count(startTime);
+//   console.log("hello");
+// };
+
+/* -------------------------------------------------------------------- */
+
+const title = document.getElementById("count-title");
+
+// function counter(i = 0) {
+//   do {
+//     title.innerHTML = i.toString();
+//     i++;
+//   } while (i <= 1e3);
+// }
+
+function counter(i = 0) {
   do {
     i++;
-  } while (i % 1e6 != 0);
-  if (i === 1e9)
-    alert(
-      "Hey bro, I've just finished it within " + (Date.now() - startTime) + "ms"
-    );
-  else {
-    console.log(i);
-    setTimeout(count);
-  }
+    title.innerHTML = i.toString();
+  } while (i % 1e3 != 0);
+  if (i != 1e4) queueMicrotask(() => counter(i));
 }
 
 callBtn.onclick = function () {
-  count();
+  counter();
+  console.log("next call");
 };
