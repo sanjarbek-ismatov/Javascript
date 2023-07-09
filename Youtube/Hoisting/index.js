62bda305cc54516f6e5e0826e497053fbedc0d4d
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-07-09 09:36:23 +0500

    learn: Hoisting

---
+++ Youtube/Hoisting/index.js
@@ -0,0 +1,13 @@
+var last = "Hello";
+
+firstname = "Sanjarbek";
+console.log(firstname);
+
+var firstname;
+
+console.log(getName());
+
+function getName() {
+  var name = "Sanjarbek";
+  return name;
+}
