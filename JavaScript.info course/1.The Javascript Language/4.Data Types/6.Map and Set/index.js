7ae49827c8c394c35fe15c285eab3452698759e1
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-07-27 11:07:37 +0500

    learn: map and set

--- JavaScript.info course/1.The Javascript Language/4.Data Types/6.Map and Set/index.js
+++ JavaScript.info course/1.The Javascript Language/4.Data Types/6.Map and Set/index.js
@@ -21,4 +21,41 @@ const user = {
     isMarried: false
 }
 const userMap = new Map(Object.entries(user))
+
+const mapToUser = Object.fromEntries(userMap.entries())
+const mapTUser2 = Object.fromEntries(userMap)
+console.log(mapToUser) // now it's object
+console.log(mapTUser2) // the same as above
 console.log(userMap)
+
+// Set
+
+const set = new Set([1, 2, 1])
+set.forEach((value, valueAgain, set) => {
+    console.log(value, valueAgain)
+})
+console.log(set)
+
+
+// function aclean(arr){
+//     const temp = []
+//     const indexes = []
+//     const sorted = arr.map(e => [...e].sort((a, b) => a.localeCompare(b)).join('').toLowerCase())
+//     for(let i = 0;i < sorted.length; i++){
+//         if(!temp.includes(sorted[i])) {
+//             temp.push(sorted[i])
+//             indexes.push(i)
+//         }
+//     }
+//     return arr.filter((e, i) => indexes.includes(i))
+// }
+
+function aclean(arr){
+    const map = new Map()
+    for(const item of arr){
+        const sorted = item.toUpperCase().split("").sort().join()
+        map.set(sorted, item)
+    }
+    return Array.from(map.values())
+}
+console.log(aclean(["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"]))
