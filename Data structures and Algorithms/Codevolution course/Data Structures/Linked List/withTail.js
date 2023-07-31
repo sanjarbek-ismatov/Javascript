039aa2152fcebc2a100e53af61e90faf7b2d3902
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-07-31 07:23:03 +0500

    data structure: linked list with tail

---
+++ Data structures and Algorithms/Codevolution course/Data Structures/Linked List/withTail.js
@@ -0,0 +1,147 @@
+class Node{
+    constructor(value){
+        this.value = value
+        this.next = null
+    }
+}
+
+class LinkedList{
+    constructor(){
+        this.head = null
+        this.tail = null
+        this.size = 0
+    }
+    // O(1)
+    append(value){
+        const node = new Node(value)
+        if(this.isEmpty()) {
+            this.head = node
+            this.tail = node
+        } else{
+            this.tail.next = node
+            this.tail = node
+        }
+        this.size++
+    }
+    prepend(value){
+        if(this.isEmpty()){
+            this.append(value)
+        } else{
+            const node = new Node(value)
+            node.next = this.head
+            this.head = node
+            this.size++
+        }
+    }
+    isEmpty(){
+        return this.getSize() === 0
+    }
+    getSize(){
+        return this.size
+    }
+    insertAt(value, index){
+        if(index < 0 || index > this.getSize()) return
+        if(this.isEmpty() || index === this.getSize()) this.append(value)
+        else if(index === 0) this.prepend(value)
+        else{
+            const node = new Node(value)
+            let prev = this.head
+            let i = 0
+            while(prev && i < index - 1){
+                prev = prev.next
+                i++
+            }
+            node.next = prev.next
+            prev.next = node
+            this.size++
+        }
+    }
+    pop(){
+        if(this.isEmpty()) return null
+        if(this.getSize() === 1) {
+            this.head = null
+            this.tail = null
+        } else {
+            let curr = this.head
+            while (curr.next !== this.tail) {
+                curr = curr.next
+            }
+            const removed = this.tail
+            curr.next = null
+            this.tail = curr
+            return removed.value
+        }
+        this.size--
+        return null
+    }
+    shift(){
+        if(this.isEmpty()) return null
+        if(this.getSize() === 1){
+            this.head = null
+            this.tail = null
+        }else {
+            const head = this.head
+            this.head = this.head.next
+            return head.value
+        }
+        this.size--
+    }
+    removeAtIndex(index){
+        if(this.isEmpty() || index < 0 || index > this.getSize() - 1) return null
+        if(this.getSize() === 1){
+            this.head = null
+            this.tail = null
+        } else{
+            let prev = this.head
+            let i = 0
+            while(prev && i < index){
+                prev = prev.next
+            }
+            const removed = prev.next.value
+            prev.next = prev.next.next
+            return removed
+        }
+        this.size--
+        return null
+    }
+    removeFromValue(value){
+        if(this.isEmpty()) return null
+        let prev = this.head
+        if(prev.value === value){
+            this.shift()
+        } else {
+            while (prev.next && prev.next.value !== value) {
+                prev = prev.next
+            }
+            const removed = prev.next.value
+            prev.next = prev.next.next
+            return removed
+        }
+    }
+    search(){}
+    display(){
+        let displayValue = "{"
+        let curr = this.head
+        if(!this.isEmpty()) {
+            while (curr.next) {
+                displayValue += curr.value + ", "
+                curr = curr.next
+            }
+            displayValue += curr.value
+        }
+        displayValue += "}"
+        console.log(displayValue)
+    }
+    reverse(){}
+}
+const list = new LinkedList()
+
+list.prepend(20)
+list.prepend(10)
+list.append(30)
+list.append(40)
+list.insertAt(15, 4)
+list.removeFromValue(10)
+// list.pop()
+// list.pop()
+list.display()
