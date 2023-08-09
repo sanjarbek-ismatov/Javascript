ab108eef65630952c56228243ccb0c089f6a368e
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-08-09 10:35:55 +0500

    call && apply

--- JavaScript.info course/1.The Javascript Language/5.Advanced working with functions/9.Call, apply, bind/index.js
+++ JavaScript.info course/1.The Javascript Language/5.Advanced working with functions/9.Call, apply, bind/index.js
@@ -15,9 +15,49 @@
 
 // CALL
 
-function sayHello(message) {
-  console.log(`${message}, ${this.name}`);
+// function sayHello(message) {
+//   console.log(`${message}, ${this.name}`);
+// }
+
+// sayHello.call({ name: "Sanjarbek" }, "Salom");
+// sayHello.call({ name: "Sarvinoz" }, "Hello");
+
+const worker = {
+  method(x) {
+    return x + 1;
+  },
+  slow(x) {
+    return this.method(x);
+  },
+};
+
+function cacheDecorator(func) {
+  const cache = new Map();
+  return function (x) {
+    const args = [].join.call(arguments);
+    console.log("🚀 ~ file: index.js:38 ~ args:", args);
+    if (cache.has(x)) return cache.get(x);
+    let result = func.call(this, x);
+    cache.set(x, result);
+    return result;
+  };
+}
+worker.slow = cacheDecorator(worker.slow);
+
+console.log(worker.slow(1, 2, 3));
+console.log(worker.slow.calls);
+// console.log(worker.slow(2));
+
+function callsCopy(func) {
+  function wrapper(...args) {
+    wrapper.calls.push(args);
+    return func.apply(this, args);
+  }
+  wrapper.calls = [];
+  return wrapper;
 }
 
-sayHello.call({ name: "Sanjarbek" }, "Salom");
-sayHello.call({ name: "Sarvinoz" }, "Hello");
+worker.slow = callsCopy(worker.slow);
+worker.slow(1, 2, 3);
+worker.slow(1, 2, 3);
+console.log(worker.slow.calls);
