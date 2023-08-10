893ea5fa7cd0e901215c671e0964e3dc11dfd8aa
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-08-10 17:06:50 +0500

    learn: binding function

--- JavaScript.info course/1.The Javascript Language/5.Advanced working with functions/10.Binding Function/index.js
+++ JavaScript.info course/1.The Javascript Language/5.Advanced working with functions/10.Binding Function/index.js
@@ -1,5 +1,18 @@
 // Binding function
-
+Function.prototype.customBind = function(context, ...args){
+    const temp = {}
+    if(context) {
+        for (let key in context) {
+            if(key !== this.name) {
+                temp[key] = context[key]
+            }
+        }
+    }
+    temp[this.name] = this
+    return () =>  {
+        return temp[this.name](...args)
+    }
+}
 const programmer = {
     name: "Sanjarbek",
     type: "Front end",
@@ -10,30 +23,36 @@ const programmer = {
 const designer = {
     name: "John",
 }
-designer.print = programmer.print.bind(programmer)
+designer.print = programmer.print.customBind({name: "Bro"})
 // programmer.print()
 // const print = programmer.print.bind(programmer)
 // print()
 
-// designer.print()
+designer.print()
 
 function sum(a, b){
     return a + b
 }
 
-const sumWithoutArg = sum.bind(null, 2, 3)
+const sumWithoutArg = sum.bind(null, 2, 3).bind(null, 3, 4)
 console.log(sumWithoutArg())
 
-Function.prototype.customBind = function(context, ...args){
-    if(context) {
-        for (let key in context) {
-            this[key] = context[key]
-        }
-    }
-    return function() {
-        console.log(this)
-       // return this(...args)
+
+const test = sum.customBind(null, 2, 3)
+// console.log(test(a,...[], ...[2]))
+
+function partial(func, ...extraArgs){
+    return function(...args){
+        return func.call(this,...extraArgs,  ...args)
     }
 }
-const test = sum.customBind(null, 2, 3)
-console.log(test())
+const userPro = {
+    name: "Sanjarbek",
+    sayHello(one, two){
+        return this.name + one + two
+    }
+}
+console.log(userPro.sayHello(1, 2))
+
+userPro.sayHello = partial(userPro.sayHello, 1)
+console.log(userPro.sayHello(2))
