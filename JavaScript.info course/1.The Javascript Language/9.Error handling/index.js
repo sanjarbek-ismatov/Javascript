f989ebb25f8fb80077f1aac3d53c10e74d43d9ae
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-08-19 13:03:57 +0500

    learn: try-catch

---
+++ JavaScript.info course/1.The Javascript Language/9.Error handling/index.js
@@ -0,0 +1,35 @@
+try{
+    // setTimeout(() => alert("There's no alert in nodejs"), 1000) // but try-catch can't catch it
+} catch{
+    console.log("Error has been caught")
+}
+try{
+    console.log(a)
+}catch(ex){
+    console.log(ex.name) // ReferenceError
+    console.log(ex.message) // a is not defined
+    console.log(ex.stack) // full message
+    console.log(ex)
+}
+
+try{
+    var a = 10
+}catch {
+//     nothing
+}
+console.log(a)
+
+try{
+    const json = '{"name": "Sanjarbek"}'
+    const user = JSON.parse(json)
+    if(!user.age) throw new SyntaxError("json doesn't have age prop")
+    new ReferenceError()
+    new TypeError()
+    new SyntaxError()
+    new Error()
+} catch (ex){
+    if(ex instanceof SyntaxError){
+        console.log(ex.constructor.name) // SyntaxError
+    }
+    throw new ReferenceError("Fucked")
+}
