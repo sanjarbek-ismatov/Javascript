58a70e7062677a962c4a50f1649b3b735d2ab6ac
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-08-28 10:33:19 +0500

    leetcode: sort bye

--- Problems/Leetcode/2724.Sort By/index.js
+++ Problems/Leetcode/2724.Sort By/index.js
@@ -1,18 +1,20 @@
 /**
  * Quick sort algorithm
  * @param {number[]} arr
+ * @param {(x: number) => number} fn
  */
-function quickSort(arr){
+function quickSort(arr, fn){
     if(arr.length <= 1) return arr
-    let pivot = arr[arr.length - 1]
+    let pivot = arr.length - 1
     const left = []
     const right = []
     for(let i = 0; i < arr.length - 1; i++){
-        if(arr[i] < pivot){
+        if(fn(arr[i]) < fn(arr[pivot])){
             left.push(arr[i])
         }else{
             right.push(arr[i])
         }
     }
-    return [...quickSort(left), pivot, ...quickSort(right)]
+    return [...quickSort(left, fn), arr[pivot], ...quickSort(right, fn)]
 }
+console.log(quickSort([{"x":1},{"x": 0},{"x": -1}], x => x.x))
