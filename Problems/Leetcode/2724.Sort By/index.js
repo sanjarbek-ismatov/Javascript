aa593b0a46eb1a25f974d8aefbf81462d55d8501
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-08-28 08:10:37 +0500

    problem

---
+++ Problems/Leetcode/2724.Sort By/index.js
@@ -0,0 +1,18 @@
+/**
+ * Quick sort algorithm
+ * @param {number[]} arr
+ */
+function quickSort(arr){
+    if(arr.length <= 1) return arr
+    let pivot = arr[arr.length - 1]
+    const left = []
+    const right = []
+    for(let i = 0; i < arr.length - 1; i++){
+        if(arr[i] < pivot){
+            left.push(arr[i])
+        }else{
+            right.push(arr[i])
+        }
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)]
+}
