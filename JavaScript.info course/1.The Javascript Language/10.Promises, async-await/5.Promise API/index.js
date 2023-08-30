a8d1e4810ebb68fcb551862a29f6732356d7bb50
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-08-30 10:29:22 +0500

    Promise API

---
+++ JavaScript.info course/1.The Javascript Language/10.Promises, async-await/5.Promise API/index.js
@@ -0,0 +1,36 @@
+const promises = [
+    new Promise((resolve, reject) => {
+        resolve(1)
+    }),
+    new Promise((resolve, reject) => {
+        resolve(4)
+    }),
+    new Promise((resolve, reject) => {
+        reject(1)
+    })
+]
+// Promise.all(promises).then(data => console.log(data))
+Promise.allSettled(promises).then(result => {
+    console.log(result)
+})
+/**
+ *  Promise.allSettled clone
+ * @param {Promise[]} promises
+ * @returns {Promise<unknown>}
+ */
+function allSettledClone(promises){
+    return new Promise((resolve, reject) => {
+        const result = []
+        for(const promise of promises){
+            promise.then(value => result.push({status: "fulfilled", value}), reason => result.push({status: "rejected", reason}))
+        }
+        resolve(result)
+    })
+}
+
+Object.defineProperty(Promise, "allSettledClone", {
+    value: allSettledClone,
+    configurable: false,
+    writable: false
+})
+Promise.allSettledClone(promises).then(result => console.log(result))
