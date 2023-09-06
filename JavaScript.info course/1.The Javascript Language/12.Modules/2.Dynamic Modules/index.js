48a6f2af20313e479dd75ee4f3779125bcc07765
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-09-06 09:21:30 +0500

    dynamic modules

---
+++ JavaScript.info course/1.The Javascript Language/12.Modules/2.Dynamic Modules/index.js
@@ -0,0 +1,4 @@
+// const jquery = await import("https://code.jquery.com/jquery-3.7.1.min.js");
+import("https://code.jquery.com/jquery-3.7.1.min.js").then((response) => {
+  console.log(response);
+});
