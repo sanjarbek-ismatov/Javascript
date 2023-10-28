26d05b4df6271b37c8819d11d7dbe1d496b3ab81
Sanjarbek <ismatovsanjarbek@yandex.ru>
2023-10-28 09:37:54 +0500

    listener got added

--- JavaScript.info course/3.Additional articles/6.Web components/Projects/Github Avatar/main.js
+++ JavaScript.info course/3.Additional articles/6.Web components/Projects/Github Avatar/main.js
@@ -6,6 +6,17 @@ class GitHubAvatarElement extends HTMLElement {
     super();
   }
 
+  render(value) {
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.append(template.content.cloneNode(true));
+    const image = this.shadowRoot.getElementById("user-image");
+    const title = this.shadowRoot.getElementById("user-name");
+    const description = this.shadowRoot.getElementById("description");
+    image.src = value["avatar_url"];
+    title.innerText = value.name;
+    description.innerText = value.bio;
+  }
+
   async request(username) {
     const response = await fetch(API_URL + username);
     return await response.json();
@@ -14,14 +25,13 @@ class GitHubAvatarElement extends HTMLElement {
   connectedCallback() {
     const username = this.getAttribute("username");
     this.request(username).then((value) => {
-      this.attachShadow({ mode: "open" });
-      this.shadowRoot.append(template.content.cloneNode(true));
-      const image = this.shadowRoot.getElementById("user-image");
-      const title = this.shadowRoot.getElementById("user-name");
-      const description = this.shadowRoot.getElementById("description");
-      image.src = value["avatar_url"];
-      title.innerText = value.name;
-      description.innerText = value.bio;
+      this.render(value);
+      const loadedEvent = new CustomEvent("loaded", {
+        composed: true,
+        bubbles: true,
+        detail: { value },
+      });
+      this.dispatchEvent(loadedEvent);
     });
   }
 }
