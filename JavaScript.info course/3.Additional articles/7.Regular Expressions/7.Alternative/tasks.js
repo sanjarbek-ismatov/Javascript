406820d84af6e2b807eb288a0d974ddf85927117
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2023-11-21 09:13:54 +0500

    regex alternation tasks are complete

--- JavaScript.info course/3.Additional articles/7.Regular Expressions/7.Alternative/tasks.js
+++ JavaScript.info course/3.Additional articles/7.Regular Expressions/7.Alternative/tasks.js
@@ -1,4 +1,10 @@
-let regexp = /\[(?<same>\w+\]).*?\[\/(\k<same>)/gi;
+// let regexp = /\[(?<same>\w+\]).*?\[\/(\k<same>)/gi;
+//
+// let str = "..[url][b]http://google.com[/b][/url]..";
+// console.log( str.match(regexp) ); // [url][b]http://google.com[/b][/url]
 
-let str = "..[url][b]http://google.com[/b][/url]..";
-console.log( str.match(regexp) ); // [url][b]http://google.com[/b][/url]
+
+const styleTagRegex = /<style(>|\s.*?>)/g
+console.log('<style> <styler> <style test="...">'.match(styleTagRegex))
+
+
