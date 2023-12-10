dcdeba22dd8915cc1bf42efad3370f302627d9c5
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2023-12-10 10:29:57 +0500

    cloner project

---
+++ projects/object cloner/cloner.js
@@ -0,0 +1,25 @@
+/*
+const key = "use";
+const user = {
+  name: "Sanjarbek",
+  age: 18,
+  sex: "male",
+  isAdult() {
+    return this.age >= 18;
+  },
+  [key + "Detail"]: "nothing",
+  girlfriend: {
+    name: "Sarvinoz",
+    age: 18,
+  },
+};
+*/
+
+{
+  key1: {
+    key2: {
+      key21: 1;
+      key22: 2;
+    }
+  }
+}
