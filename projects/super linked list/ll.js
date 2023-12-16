42d262da28a7d027fe9981bae04f1527d12842fd
sanjarbek-ismatov <ismatovsanjarbek@yandex.ru>
2023-12-16 10:28:24 +0500

    super linked list is working

--- projects/super linked list/ll.js
+++ projects/super linked list/ll.js
@@ -4,8 +4,10 @@ function Node(data) {
   this.prev = null;
   this.next = null;
 }
-function LinkedList() {
+function LinkedList(array) {
   if (!new.target) {
+    const ll = new LinkedList();
+    return ll.fromArraytoLList(array);
   }
   this.head = null;
   this.tail = null;
@@ -13,16 +15,51 @@ function LinkedList() {
     return this.head === null;
   };
   this.append = function (data) {
-    let curr = this.head;
-    while (curr.next) {
-      curr = curr.next;
+    if (this.isEmpty()) this.prepend(data);
+    else {
+      const node = new Node(data);
+      this.tail.next = node;
+      node.prev = this.tail;
+      this.tail = node;
     }
-    curr.next = new Node(data);
   };
   this.prepend = function (data) {
     const node = new Node(data);
-    const currentHead = this.head;
-    node.next = currentHead;
-    this.head = node;
+    if (this.isEmpty()) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      node.next = this.head;
+      this.head.prev = node;
+      this.head = node;
+    }
+  };
+  this.toViewAsArray = function () {
+    return Array.from(this[Symbol.iterator]());
+  };
+  this.fromArraytoLList = function (array) {
+    const tempList = new LinkedList();
+    for (let item of array) {
+      tempList.append(item);
+    }
+    return tempList;
+  };
+  this[Symbol.iterator] = function* () {
+    let curr = this.head;
+    while (curr) {
+      yield curr.value;
+      curr = curr.next;
+    }
   };
 }
+
+const ll = new LinkedList();
+ll.append(10);
+ll.append(20);
+ll.prepend(0);
+// for (let i of ll) {
+//   console.log(i);
+// }
+const array = ll.toViewAsArray();
+const arrayAsLList = LinkedList(array);
+console.log(arrayAsLList.toViewAsArray());
